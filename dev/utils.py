4e4aa16d861823b513dd43a5d64a268b1c49bfa5
mr oCean <drcjwilson+git@gmail.com>
2023-07-26 12:20:18 +0100

    ongoing testing in notebooks; mockup config file added; gitignore updated to ignore hidden directories

---
+++ dev/utils.py
@@ -0,0 +1,22 @@
+import re
+
+def init_vocab():
+    pass
+
+def options_db_to_global_attrs():
+    pass
+
+def standardise_units(units:str=''):
+    """
+    Find any slashes and corresponding exponents and replace.
+    e.g. 'kg/m^2/s' will become 'kg m-2 s-1'
+    """
+    # Check for units cancelling out (e.g. replace kg/kg with 1)
+
+    # Use RegEx to find slashes and corresponding exponents.
+    
+    
+
+    
+    pass
+
