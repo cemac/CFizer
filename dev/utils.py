cd163fb0ff3c59af40f2ca6c7a38a424ecfab4ab
mr oCean <drcjwilson+git@gmail.com>
2023-08-18 16:29:14 +0100

    Stopped performance timer from outputting objects passed as arguments

--- dev/utils.py
+++ dev/utils.py
@@ -13,7 +13,7 @@ def performance_time(func):
 		response = func(*args, **kwargs)  # run the wrapped function
 		end_time = perf_counter()
 		duration = end_time - start_time
-		print(f"{func}({args}, {kwargs}) took {duration} seconds.")
+		print(f"{func} took {duration} seconds.")  # ({args}, {kwargs})
 		return response
 	return wrapper
 
