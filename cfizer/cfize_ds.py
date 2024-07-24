202050c4da086ff93edef129fa14bd226b615685
Anne Barber <lmkk419@leeds.ac.uk>
2024-07-24 15:41:09 +0100

    Added 'squeeze=False' to xarray.Dataset.groupby() statement in order to preserve the time dimension when datasets are split - see issue #58 for further details

--- cfizer/cfize_ds.py
+++ cfizer/cfize_ds.py
@@ -52,7 +52,7 @@ def split_ds(
             f"splitting dataset {dataset.attrs['title']} by {var}."
         )
     base_title = dataset.attrs["title"].strip("_ +,.&") + "_"
-    grouped = {point: ds.copy(deep=True) for (point, ds) in dataset.groupby(var)}
+    grouped = {point: ds.copy(deep=True) for (point, ds) in dataset.groupby(var, squeeze=False)}
     for point, ds in grouped.items():
         # Append relevant coordinate value to title
         grouped[point].attrs["title"] = base_title + str(int(point))
